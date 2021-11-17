9635ac7af757cf8f0b35dc445de059f0ae9b19ee
Richard Gomez <rmgomez368@gmail.com>
2021-11-17 11:50:24 -0500

    feat: simplify return type

--- src/main/kotlin/org/acme/ReactiveGreetingResource.kt
+++ src/main/kotlin/org/acme/ReactiveGreetingResource.kt
@@ -5,12 +5,10 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-data class Greeting(val message: String = "Hello, world!")
-
 @Path("/hello")
 class ReactiveGreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    suspend fun hello(): Greeting = Greeting()
+    suspend fun hello(): String = "Hello"
 }
